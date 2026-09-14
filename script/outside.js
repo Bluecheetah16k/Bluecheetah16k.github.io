fbfb8c6efc1d27779c4e7f73d3ff9a4277b1b410
Bluecheetah16k <pirniew9365@my.sduhsd.net>
2026-09-14 12:37:34 -0700

    Reduce delays and adjust resource values for workers

--- script/outside.js
+++ script/outside.js
@@ -13,84 +13,84 @@ var Outside = {
 	_INCOME: {
 		'gatherer': {
 			name: _('gatherer'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'wood': 1
+				'wood': 0.1
 			}
 		},
 		'hunter': {
 			name: _('hunter'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'fur': 0.5,
-				'meat': 0.5
+				'fur': 0.05,
+				'meat': 0.05
 			}
 		},
 		'trapper': {
 			name: _('trapper'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'meat': -1,
-				'bait': 1
+				'meat': -0.1,
+				'bait': 0.1
 			}
 		},
 		'tanner': {
 			name: _('tanner'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'fur': -5,
-				'leather': 1
+				'fur': -0.5,
+				'leather': 0.1
 			}
 		},
 		'charcutier': {
 			name: _('charcutier'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'meat': -5,
-				'wood': -5,
-				'cured meat': 1
+				'meat': -0.5,
+				'wood': -0.5,
+				'cured meat': 0.1
 			}
 		},
 		'iron miner': {
 			name: _('iron miner'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'cured meat': -1,
+				'cured meat': -0.1,
 				'iron': 1
 			}
 		},
 		'coal miner': {
 			name: _('coal miner'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'cured meat': -1,
-				'coal': 1
+				'cured meat': -0.1,
+				'coal': 0.1
 			}
 		},
 		'sulphur miner': {
 			name: _('sulphur miner'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'cured meat': -1,
-				'sulphur': 1
+				'cured meat': -0.1,
+				'sulphur': 0.1
 			}
 		},
 		'steelworker': {
 			name: _('steelworker'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'iron': -1,
-				'coal': -1,
-				'steel': 1
+				'iron': -0.1,
+				'coal': -0.1,
+				'steel': 0.1
 			}
 		},
 		'armourer': {
 			name: _('armourer'),
-			delay: 10,
+			delay: 1,
 			stores: {
-				'steel': -1,
-				'sulphur': -1,
-				'bullets': 1
+				'steel': -0.1,
+				'sulphur': -0.1,
+				'bullets': 0.1
 			}
 		}
 	},
